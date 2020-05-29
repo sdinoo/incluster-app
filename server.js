d88f8b001a6d03dcee87f219b9ba6faea840a2c4
Dinesh Shetty <dineshshetty@Dineshs-MacBook-Pro.local>
2020-05-30 00:15:07 +0530

    trying to list configmaps

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const k8s = require('@kubernetes/client-node');
 
 const kc = new k8s.KubeConfig();
 kc.loadFromCluster();
-console.log("k8s", k8s.CoreV1Api);
-console.log("kc", kc);
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 // This is a new comment i want to see get pushed into github
-k8sApi.listNamespacedPod('api-hub-operator-project')
+kc.
+k8sApi.listConfigMapForAllNamespaces('dines-example')
     .then((res) => {
 	console.log(res.body);
     })
