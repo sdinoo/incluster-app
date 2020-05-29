79b32eb501ce5a5e7f0a1f51cca06a3c9055dcec
Dinesh Shetty <dineshshetty@Dineshs-MacBook-Pro.local>
2020-05-29 23:27:02 +0530

    changed to default

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const k8s = require('@kubernetes/client-node');
 
 const kc = new k8s.KubeConfig();
-kc.loadFromCluster();
+kc.loadFromDefault();
 
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 // This is a new comment i want to see get pushed into github
