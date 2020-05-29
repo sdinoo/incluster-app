30197214a41cf9b9ad7a183a1e8424dbd6a7ba8a
Dinesh Shetty <dineshshetty@Dineshs-MacBook-Pro.local>
2020-05-29 23:51:56 +0530

    changed namespace

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const k8s = require('@kubernetes/client-node');
 
 const kc = new k8s.KubeConfig();
-kc.loadFromDefault();
+kc.loadFromCluster();
 console.log("k8s", k8s.CoreV1Api);
 console.log("kc", kc);
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 // This is a new comment i want to see get pushed into github
-k8sApi.listNamespacedPod('default')
+k8sApi.listNamespacedPod('api-hub-operator-project')
     .then((res) => {
 	console.log(res.body);
     })
