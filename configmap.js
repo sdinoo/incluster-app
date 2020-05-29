a0d2092946996aa7a739a4b18b644f24ddb35b1f
Dinesh Shetty <dineshshetty@Dineshs-MacBook-Pro.local>
2020-05-30 00:37:48 +0530

    async

--- configmap.js
+++ configmap.js
@@ -7,23 +7,26 @@ try {
 }
 client = new kubernetes.Client({ config })
 
-await client.loadSpec()
 
-let apiRes
-if (client.apis.apps.v1) {
-    apiRes = await client.apis
-        .apps
-        .v1
-        .namespaces(req.params.namespace)
-        .deployments(req.params.deployment)
-        .get()
-} else {
-    apiRes = await client.apis
-        .extensions
-        .v1beta1
-        .namespaces(req.params.namespace)
-        .deployments(req.params.deployment)
-        .get()
-}
 
-console.log("API RES BODY", apiRes.body);
+    (async function () {
+        await client.loadSpec()
+        let apiRes
+        if (client.apis.apps.v1) {
+            apiRes = await client.apis
+                .apps
+                .v1
+                .namespaces(req.params.namespace)
+                .deployments(req.params.deployment)
+                .get()
+        } else {
+            apiRes = await client.apis
+                .extensions
+                .v1beta1
+                .namespaces(req.params.namespace)
+                .deployments(req.params.deployment)
+                .get()
+        }
+
+        console.log("API RES BODY", apiRes.body);
+    })()
