4135e3faddb318f727a29ec684250c952bcd267a
Dinesh Shetty <dineshshetty@Dineshs-MacBook-Pro.local>
2020-05-30 11:39:03 +0530

    trying again

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const kc = new k8s.KubeConfig();
 kc.loadFromCluster();
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 // This is a new comment i want to see get pushed into github
-kc.
 k8sApi.listNamespacedPod('api-hub-operator-project')
     .then((res) => {
 	console.log(res.body);
