d3ff33be087bb80e4e2cc13b58bf2c41eff925f0
Dinesh Shetty <dineshshetty@Dineshs-MacBook-Pro.local>
2020-05-30 15:45:59 +0530

    asd

--- serverv2.js
+++ serverv2.js
@@ -1,10 +1,9 @@
 const k8s = require('@kubernetes/client-node');
 const kc = new k8s.KubeConfig();
-// kc.loadFromCluster();
-kc.loadFromDefault();
+kc.loadFromCluster();
+//kc.loadFromDefault();
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 const sf = kc.makeApiClient(k8s.CoreV1Api);
-
 k8sApi.listNamespacedConfigMap('api-hub-operator-project').then((res) => {
     console.log("configmaps: ", res.body.items);
     for (const configmap of res.body.items) {
