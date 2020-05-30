b2636674516b9e9c367a17cd55bdbace038e7534
Dinesh Shetty <dineshshetty@Dineshs-MacBook-Pro.local>
2020-05-30 15:26:30 +0530

    khj

--- serverv2.js
+++ serverv2.js
@@ -6,7 +6,7 @@ const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 const sf = kc.makeApiClient(k8s.CoreV1Api);
 
 k8sApi.listNamespacedConfigMap('api-hub-operator-project').then((res) => {
-    // console.log("configmaps: ", res.body.items);
+    console.log("configmaps: ", res.body.items);
     for (const configmap of res.body.items) {
         console.log("", configmap.data)
     }
